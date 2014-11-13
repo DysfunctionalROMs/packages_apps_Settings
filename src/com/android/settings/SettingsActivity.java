a2eaa560cb3dc5480ca59bc5d58fd3421c85e543
Danny Baumann <dannybaumann@web.de>
2014-11-12 17:14:27 -0800

    Add back blacklist settings. [Squashed]
    Cm originally had this squashed, but with the mess that is cm-13 even theyre
    squashed became unsquashed and you had to dig in like 10 commits that had
    nothing to do with blacklist to find all the code. thats what happens when
    one person merges fwbase and another setting.. c'mon man organize your shit!
    
    Change-Id: I45d6c90bd584d65c83504a0fdca96f69f4ee7d94

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -78,6 +78,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.broken.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
@@ -357,7 +358,8 @@ public class SettingsActivity extends Activity
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
             NavigationSettings.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            BlacklistSettings.class.getName()
     };
 
 
