75b98690df14e83ffa08a7fc3fa6520822d6a71e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-11-15 15:25:51 -0500

    Settings: add "advanced" menu to Display/TapToWake
              Forward TapToWake from CM11
    
              Contains Commits:
    
              Author: Ricardo Cerqueira<cyanogenmod@cerqueira.org>
              DisplaySettings: Add hardware-framework support for tap-to-wake
    
              Since we have tap-to-sleep globally, might as well support the
              wake gesture as a standalone thing through the HAF. Place it in
              "Display".
              Change-Id: I6facc2334ff9e80077581c54c428476594e91528
    
              Author: Roman Birg <roman@cyngn.com>
              Settings: set proper default value for tap to wake
    
              The first time the display_settings.xml file gets inflated, the
              PreferenceManager takes the defaultValue and persists it in the default
              shared preferences file.
              When we read the double tap to wake preference,
              we assume it has a default value of true, which it uses until the user
              enters Display Settings for the first time. At that point, the default
              value is persisted, and every boot after will use the default value in
              display_settings.xml.
              Remove the default defined in the XML file.
              Change-Id: I0989dd73b6b3c42ff8649bc17eebf4e191293ee4
    
    Change-Id: I271390aa7bfc56811dcf6fff7e1d05c76c4bfa48
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java
    
    Conflicts:
    	res/values/broken_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java
    
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -37,6 +37,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Build;
@@ -46,6 +47,8 @@ import android.os.SystemProperties;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
@@ -58,6 +61,8 @@ import com.android.settings.hardware.DisplayGamma;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.cyanogenmod.hardware.TapToWake;
 
 public class DisplaySettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, OnPreferenceClickListener, Indexable {
@@ -76,6 +81,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
+    private static final String KEY_TAP_TO_WAKE = "double_tap_wake_gesture";
+
+    private static final String CATEGORY_ADVANCED = "advanced_display_prefs";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -88,6 +96,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mLiftToWakePreference;
     private SwitchPreference mDozePreference;
     private SwitchPreference mAutoBrightnessPreference;
+    private SwitchPreference mTapToWake;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -170,13 +179,19 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             });
         }
 
-        mScreenColorSettings = (PreferenceScreen) findPreference(KEY_SCREEN_COLOR_SETTINGS);
-
         if (!DisplayColor.isSupported()) {
             getPreferenceScreen().removePreference(findPreference(KEY_DISPLAY_COLOR));
         }
         if (!DisplayGamma.isSupported()) {
             getPreferenceScreen().removePreference(findPreference(KEY_DISPLAY_GAMMA));
+        }
+
+        PreferenceScreen advancedPrefs = (PreferenceScreen) findPreference(CATEGORY_ADVANCED);
+
+        mTapToWake = (SwitchPreference) findPreference(KEY_TAP_TO_WAKE);
+        if (!isTapToWakeSupported()) {
+            advancedPrefs.removePreference(mTapToWake);
+            mTapToWake = null;
         }
     }
 
@@ -304,6 +319,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+
+        if (mTapToWake != null) {
+            mTapToWake.setChecked(TapToWake.isEnabled());
+        }
+
         updateState();
     }
 
@@ -352,6 +372,15 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
     }
 
+    private static boolean isTapToWakeSupported() {
+        try {
+            return TapToWake.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed
+            return false;
+        }
+    }
+
     public void writeFontSizePreference(Object objValue) {
         try {
             mCurConfig.fontScale = Float.parseFloat(objValue.toString());
@@ -363,6 +392,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mTapToWake) {
+            return TapToWake.setEnabled(mTapToWake.isChecked());
+        }
+
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
@@ -408,6 +441,24 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             }
         }
         return false;
+    }
+
+
+    /**
+     * Restore the properties associated with this preference on boot
+       @param ctx A valid context
+     */
+    public static void restore(Context ctx) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+        if (isTapToWakeSupported()) {
+            final boolean enabled = prefs.getBoolean(KEY_TAP_TO_WAKE,
+                TapToWake.isEnabled());
+            if (!TapToWake.setEnabled(enabled)) {
+                Log.e(TAG, "Failed to restore tap-to-wake settings.");
+            } else {
+                Log.d(TAG, "Tap-to-wake settings restored.");
+            }
+        }
     }
 
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
