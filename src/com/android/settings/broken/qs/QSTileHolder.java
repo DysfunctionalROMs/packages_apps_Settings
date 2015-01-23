8a39dbd239e375086b2bbe864111207dd1d3b760
Danesh M <daneshm90@gmail.com>
2015-01-23 11:57:03 -0800

    Settings : Add adb over network tile
    Change-Id: I6c30ff1d40dddc01a52e450b860351fca4123fe4
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/broken/qs/QSTileHolder.java
+++ src/com/android/settings/broken/qs/QSTileHolder.java
@@ -95,6 +95,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_hotspot_on";
                 stringId = R.string.hotspot;
                 break;
+            case QSConstants.TILE_ADB_NETWORK:
+                resourceName = "ic_qs_network_adb_on";
+                stringId = R.string.qs_tile_adb_over_network;
+                break;
             default:
                 return null;
         }
