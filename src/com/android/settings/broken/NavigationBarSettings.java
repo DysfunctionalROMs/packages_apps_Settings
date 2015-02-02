620c37e40fa3bf485bdf43a1b835f22e01fa061e
lichti1901 <lichti190185@gmail.com>
2015-02-02 19:42:49 +0100

    Navigation bar button color (2/2)
    Conflicts:
    	res/values/custom_strings.xml

--- src/com/android/settings/broken/NavigationBarSettings.java
+++ src/com/android/settings/broken/NavigationBarSettings.java
@@ -1,6 +1,7 @@
 
 package com.android.settings.broken;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -10,15 +11,21 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class NavigationBarSettings extends SettingsPreferenceFragment implements
 OnPreferenceChangeListener {
 
     private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
 
+    private static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+
     private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
     private ListPreference mNavigationBarHeight;
+    private ColorPickerPreference mNavbarButtonTint;
     private SwitchPreference mKillAppLongPressBack;
 
     @Override
@@ -35,6 +42,14 @@ OnPreferenceChangeListener {
         mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
         mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
 
+        mNavbarButtonTint = (ColorPickerPreference) findPreference(NAVIGATION_BAR_TINT);
+        mNavbarButtonTint.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_TINT, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mNavbarButtonTint.setSummary(hexColor);
+        mNavbarButtonTint.setNewPreviewColor(intColor);
+
         mKillAppLongPressBack = (SwitchPreference) findPreference(KILL_APP_LONGPRESS_BACK);
         mKillAppLongPressBack.setOnPreferenceChangeListener(this);
         int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
@@ -50,8 +65,17 @@ OnPreferenceChangeListener {
                     Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
             mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
             return true;
-        }
-        else if (preference == mKillAppLongPressBack) {
+
+        } else if (preference == mNavbarButtonTint) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_TINT, intHex);
+            return true;
+
+        } else if (preference == mKillAppLongPressBack) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), 
                     KILL_APP_LONGPRESS_BACK, value ? 1 : 0);
