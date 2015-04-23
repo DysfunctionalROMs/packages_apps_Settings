e0bed8da38c35f4b25340397f39a42b976d58d69
FuckingShitUp <kengreenwood420666@gmail.com>
2015-04-23 13:01:58 -0500

    Breaking a few things to fix build

--- src/com/android/settings/broken/NavBar.java
+++ src/com/android/settings/broken/NavBar.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.terminus;
+package com.android.settings.broken;
 
 import android.app.Activity;
 import android.app.AlertDialog;
