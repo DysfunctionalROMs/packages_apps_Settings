lichti1901/android_packages_apps_settings

Customizeable QS Tiles (2/2) Squashed
Thanks to CyanogenMod, Euphoria OS and all the others who contributed!

Add customization for Quick Settings [2/2]

Add a new section to customize Quick Settings.

Modified DraggableGridView to arrange the items differently.

Gave everything a "Material-ish" style.

Inversion and Hotspot tiles can be rearranged now.

Creadted a "Notification Drawer" category.

QS: change location sensors mode in quick settings [2/2]
Added switch in notification drawer settings
to enable advanced setting for location tile

add missing string for Rotation tile

add data and notification tile

remove notification tile

QS: Longclick action [2/2]
Long press expanded status bar Settings button opens tiles settings

Thanks to @romanbb for status bar weather code & @Altaf-Mahdi for Status Bar Header haptic feedback option

Signed-off-by: snak3ater <saeedyasir91@gmail.com>

Cleanup and fix QS tile implementation

- Decouple DraggableGridView and QSTiles code by shifting around some
  logic
- Fix column-for-position calculation
- Improve layouts and preference ordering

Change-Id: I7a06150f2602888b0f28fca93e0164e381272b67

QS: some corrections on location tile code (2/2)

Setting moved to Secure

Wifi: add option to show quick settings detail view (2/2)

Added a switch preference in notification drawer settings
to enable detail view for Wi-Fi tile

Settings : Sort QSTiles in picker

Change-Id: I677226a456ee8673860d4de291ba435d3ad00cbb

switch to SecureSwitchpreference

Settings : Update for centralized quick settings

Change-Id: If9cad4fd45e5bf7deb13aa1a5284ac5566e44a95

Cleanup QS.

Settings: add newly added toggles

Change-Id: I338eae4546bbc6c9f8453141bf2a8e5afb41fa7b
Signed-off-by: Roman Birg <roman@cyngn.com>

Properly name string resources.

Remove Lockscreen tile. Nobody needs that

QS: add Data tile and cleanup

U nasty little bastard ....
damn this one sucks my nerves
Fix QS Tiles

Quick Settings: Auto collapse panel (2/2)

Signed-off-by: Altaf-M

src/com/android/settings/Settings.java
@@ -17,6 +17,7 @@
 package com.android.settings;
 
 import com.android.settings.applications.AppOpsCategory;
+import com.android.settings.terminus.qs.QSTiles;
 
 /**
  * Top-level Settings activity
@@ -101,5 +102,7 @@ public class Settings extends SettingsActivity {
 
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/terminus/NotificationDrawerSettings.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.terminus;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.terminus.qs.QSTiles;
+
+public class NotificationDrawerSettings extends SettingsPreferenceFragment {
+    private Preference mQSTiles;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.notification_drawer_settings);
+
+        mQSTiles = findPreference("qs_order");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int qsTileCount = QSTiles.determineTileCount(getActivity());
+        mQSTiles.setSummary(getResources().getQuantityString(R.plurals.qs_tiles_summary,
+                    qsTileCount, qsTileCount));
+    }
+}

src/com/android/settings/terminus/qs/ColoringCardView.java
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.terminus.qs;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class ColoringCardView extends CardView {
+    private PorterDuffColorFilter mColorFilter;
+    private Paint mPaint;
+
+    public ColoringCardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setColor(int color) {
+        if (color != mColorFilter.getColor()) {
+            mColorFilter.setColor(color);
+            mPaint.setColorFilter(color == Color.TRANSPARENT ? null : mColorFilter);
+            setLayerPaint(mPaint);
+        }
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mColorFilter = new PorterDuffColorFilter(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY);
+        setLayerType(LAYER_TYPE_HARDWARE, mPaint);
+    }
+}
+

src/com/android/settings/terminus/qs/QSTileHolder.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.terminus.qs;
+
+import android.content.Context;
+
+import com.android.internal.util.cm.QSConstants;
+import com.android.internal.util.cm.QSUtils;
+import com.android.settings.R;
+
+/**
+ * This class holds the icon, the name - or the string the user sees,
+ * and the value which will be stored
+ */
+public class QSTileHolder {
+    public static final String TILE_ADD_DELETE = "";
+
+    public final String resourceName;
+    public final String value;
+    public final String name;
+
+    public QSTileHolder(String resourceName, String value, String name) {
+        this.resourceName = resourceName;
+        this.value = value;
+        this.name = name;
+    }
+
+    public static QSTileHolder from(Context context, String tileType) {
+        String resourceName = null;
+        int stringId = -1;
+
+        if (!TILE_ADD_DELETE.equals(tileType) &&
+                !QSUtils.getAvailableTiles(context).contains(tileType)) {
+            return null;
+        }
+
+        switch (tileType) {
+            case TILE_ADD_DELETE:
+                break;
+            case QSConstants.TILE_WIFI:
+                resourceName = "ic_qs_wifi_full_4";
+                stringId = R.string.qs_tile_wifi;
+                break;
+            case QSConstants.TILE_BLUETOOTH:
+                resourceName = "ic_qs_bluetooth_on";
+                stringId = R.string.qs_tile_bluetooth;
+                break;
+            case QSConstants.TILE_INVERSION:
+                resourceName = "ic_qs_inversion_on";
+                stringId = R.string.qs_tile_display_inversion;
+                break;
+            case QSConstants.TILE_CELLULAR:
+                resourceName = "ic_qs_signal_full_4";
+                stringId = R.string.qs_tile_cellular_data;
+                break;
+            case QSConstants.TILE_AIRPLANE:
+                resourceName = "ic_qs_airplane_on";
+                stringId = R.string.qs_tile_airplane_mode;
+                break;
+            case QSConstants.TILE_ROTATION:
+                resourceName = "ic_qs_rotation_locked";
+                stringId = R.string.qs_tile_display_rotation;
+                break;
+            case QSConstants.TILE_FLASHLIGHT:
+                resourceName = "ic_qs_flashlight_on";
+                stringId = R.string.qs_tile_flashlight;
+                break;
+            case QSConstants.TILE_LOCATION:
+                resourceName = "ic_qs_location_on";
+                stringId = R.string.qs_tile_location;
+                break;
+            case QSConstants.TILE_CAST:
+                resourceName = "ic_qs_cast_on";
+                stringId = R.string.qs_tile_cast_screen;
+                break;
+            case QSConstants.TILE_HOTSPOT:
+                resourceName = "ic_qs_hotspot_on";
+                stringId = R.string.qs_tile_hotspot;
+                break;
+            case QSConstants.TILE_NOTIFICATIONS:
+                resourceName = "ic_qs_ringer_audible";
+                stringId = R.string.qs_tile_notifications;
+                break;
+            case QSConstants.TILE_NFC:
+                resourceName = "ic_qs_nfc_on";
+                stringId = R.string.qs_tile_nfc;
+                break;
+            case QSConstants.TILE_COMPASS:
+                resourceName = "ic_qs_compass_on";
+                stringId = R.string.qs_tile_compass;
+                break;
+            case QSConstants.TILE_DATA:
+                resourceName = "ic_qs_data_on";
+                stringId = R.string.qs_tile_data;
+                break;
+            case QSConstants.TILE_BATTERY_SAVER:
+                resourceName = "ic_qs_battery_saver_on";
+                stringId = R.string.qs_battery_saver_tile;
+                break;
+            case QSConstants.TILE_BRIGHTNESS:
+                resourceName = "ic_qs_brightness_auto_off_alpha";
+                stringId = R.string.qs_brightness_tile;
+                break;
+            case QSConstants.TILE_EXPANDED_DESKTOP:
+                resourceName = "ic_qs_expanded_desktop";
+                stringId = R.string.qs_expanded_desktop_tile;
+                break;
+            default:
+                return null;
+        }
+
+        String name = stringId != -1 ? context.getString(stringId) : null;
+        return new QSTileHolder(resourceName, tileType, name);
+    }
+}

DysfunctionalROMs/packages_apps_Settings

Conflicts:
	res/values/custom_colors.xml
	res/values/custom_dimens.xml
	res/values/custom_strings.xml
	Android.mk
	res/values/custom_plurals.xml
	res/xml/status_bar_settings.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/Settings.java
@@ -17,6 +17,7 @@
 package com.android.settings;
 
 import com.android.settings.applications.AppOpsSummary;
+import com.android.settings.broken.qs.QSTiles;
 
 /**
  * Top-level Settings activity
@@ -103,5 +104,7 @@ public class Settings extends SettingsActivity {
     
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/broken/NotificationDrawerSettings.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.broken;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.broken.qs.QSTiles;
+
+public class NotificationDrawerSettings extends SettingsPreferenceFragment {
+    private Preference mQSTiles;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.notification_drawer_settings);
+
+        mQSTiles = findPreference("qs_order");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int qsTileCount = QSTiles.determineTileCount(getActivity());
+        mQSTiles.setSummary(getResources().getQuantityString(R.plurals.qs_tiles_summary,
+                    qsTileCount, qsTileCount));
+    }
+}

src/com/android/settings/broken/qs/ColoringCardView.java
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.broken.qs;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class ColoringCardView extends CardView {
+    private PorterDuffColorFilter mColorFilter;
+    private Paint mPaint;
+
+    public ColoringCardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setColor(int color) {
+        if (color != mColorFilter.getColor()) {
+            mColorFilter.setColor(color);
+            mPaint.setColorFilter(color == Color.TRANSPARENT ? null : mColorFilter);
+            setLayerPaint(mPaint);
+        }
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mColorFilter = new PorterDuffColorFilter(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY);
+        setLayerType(LAYER_TYPE_HARDWARE, mPaint);
+    }
+}
+

src/com/android/settings/broken/qs/QSTileHolder.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.broken.qs;
+
+import android.content.Context;
+
+import com.android.internal.util.cm.QSConstants;
+import com.android.internal.util.cm.QSUtils;
+import com.android.settings.R;
+
+/**
+ * This class holds the icon, the name - or the string the user sees,
+ * and the value which will be stored
+ */
+public class QSTileHolder {
+    public static final String TILE_ADD_DELETE = "";
+
+    public final String resourceName;
+    public final String value;
+    public final String name;
+
+    public QSTileHolder(String resourceName, String value, String name) {
+        this.resourceName = resourceName;
+        this.value = value;
+        this.name = name;
+    }
+
+    public static QSTileHolder from(Context context, String tileType) {
+        String resourceName = null;
+        int stringId = -1;
+
+        if (!TILE_ADD_DELETE.equals(tileType) &&
+                !QSUtils.getAvailableTiles(context).contains(tileType)) {
+            return null;
+        }
+
+        switch (tileType) {
+            case TILE_ADD_DELETE:
+                break;
+            case QSConstants.TILE_WIFI:
+                resourceName = "ic_qs_wifi_full_4";
+                stringId = R.string.qs_tile_wifi;
+                break;
+            case QSConstants.TILE_BLUETOOTH:
+                resourceName = "ic_qs_bluetooth_on";
+                stringId = R.string.qs_tile_bluetooth;
+                break;
+            case QSConstants.TILE_INVERSION:
+                resourceName = "ic_qs_inversion_on";
+                stringId = R.string.qs_tile_display_inversion;
+                break;
+            case QSConstants.TILE_CELLULAR:
+                resourceName = "ic_qs_signal_full_4";
+                stringId = R.string.qs_tile_cellular_data;
+                break;
+            case QSConstants.TILE_AIRPLANE:
+                resourceName = "ic_qs_airplane_on";
+                stringId = R.string.qs_tile_airplane_mode;
+                break;
+            case QSConstants.TILE_ROTATION:
+                resourceName = "ic_qs_rotation_locked";
+                stringId = R.string.qs_tile_display_rotation;
+                break;
+            case QSConstants.TILE_FLASHLIGHT:
+                resourceName = "ic_qs_flashlight_on";
+                stringId = R.string.qs_tile_flashlight;
+                break;
+            case QSConstants.TILE_LOCATION:
+                resourceName = "ic_qs_location_on";
+                stringId = R.string.qs_tile_location;
+                break;
+            case QSConstants.TILE_CAST:
+                resourceName = "ic_qs_cast_on";
+                stringId = R.string.qs_tile_cast_screen;
+                break;
+            case QSConstants.TILE_HOTSPOT:
+                resourceName = "ic_qs_hotspot_on";
+                stringId = R.string.qs_tile_hotspot;
+                break;
+            case QSConstants.TILE_NOTIFICATIONS:
+                resourceName = "ic_qs_ringer_audible";
+                stringId = R.string.qs_tile_notifications;
+                break;
+            case QSConstants.TILE_NFC:
+                resourceName = "ic_qs_nfc_on";
+                stringId = R.string.qs_tile_nfc;
+                break;
+            case QSConstants.TILE_COMPASS:
+                resourceName = "ic_qs_compass_on";
+                stringId = R.string.qs_tile_compass;
+                break;
+            case QSConstants.TILE_DATA:
+                resourceName = "ic_qs_data_on";
+                stringId = R.string.qs_tile_data;
+                break;
+            case QSConstants.TILE_BATTERY_SAVER:
+                resourceName = "ic_qs_battery_saver_on";
+                stringId = R.string.qs_battery_saver_tile;
+                break;
+            case QSConstants.TILE_BRIGHTNESS:
+                resourceName = "ic_qs_brightness_auto_off_alpha";
+                stringId = R.string.qs_brightness_tile;
+                break;
+            case QSConstants.TILE_EXPANDED_DESKTOP:
+                resourceName = "ic_qs_expanded_desktop";
+                stringId = R.string.qs_expanded_desktop_tile;
+                break;
+            default:
+                return null;
+        }
+
+        String name = stringId != -1 ? context.getString(stringId) : null;
+        return new QSTileHolder(resourceName, tileType, name);
+    }
+}