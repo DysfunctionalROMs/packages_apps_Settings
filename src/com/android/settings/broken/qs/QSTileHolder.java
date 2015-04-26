cbea1e7b2c55c302eea45f22ab098c251f0f876a
Noeljunior <liamgliam@gmail.com>
2015-04-26 11:48:05 +0100

    QS: Add Ambient Display Tile (2/2)
    Change-Id: I332ac0b9d69d7c3c8029511fd97ae81277ebe598
    
    Conflicts:
    	res/values/slim_strings.xml

--- src/com/android/settings/broken/qs/QSTileHolder.java
+++ src/com/android/settings/broken/qs/QSTileHolder.java
@@ -138,6 +138,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_media_play";
                 stringId = R.string.qs_music_play_tile;
                 break;
+            case QSConstants.TILE_AMBIENT_DISPLAY:
+                resourceName = "ic_qs_ambientdisplay_on";
+                stringId = R.string.qs_ambient_display;
+                break;
             default:
                 return null;
         }
