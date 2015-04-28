116a512251de68d6cbd5a6dd2a964d2dc6aa892e
FuckingShitUp <kengreenwood420666@gmail.com>
2015-04-28 18:16:57 -0500

    Allow cmhw shit like keydisabler

--- Android.mk
+++ Android.mk
@@ -32,6 +32,9 @@ LOCAL_AAPT_FLAGS := \
     --extra-packages android.support.v7.cardview
 
 include frameworks/opt/setupwizard/navigationbar/common.mk
+
+LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
+
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
