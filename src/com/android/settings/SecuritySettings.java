362e793c5f54bf4d4928da0baeb054a78261e58f
FuckingShitUp <kengreenwood420666@gmail.com>
2015-05-08 09:10:40 -0500

    Revert "Rework: Disable Quick pulldown on lockscreen"
    This reverts commit c8f0346e9e605eaa5b5be82c36dda92a735c2ee9.
    
    Conflicts:
    	res/values/custom_strings.xml

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -116,7 +116,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     // Only allow one trust agent on the platform.
     private static final boolean ONLY_ONE_TRUST_AGENT = true;
-    private static final String PREF_BLOCK_ON_SECURE_KEYGUARD = "block_on_secure_keyguard";
 
     private DevicePolicyManager mDPM;
     private SubscriptionManager mSubscriptionManager;
@@ -143,8 +142,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private boolean mIsPrimary;
 
     private Intent mTrustAgentClickIntent;
-
-    private SwitchPreference mBlockOnSecureKeyguard;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -353,17 +350,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 Settings.System.LOCK_TO_APP_ENABLED, 0) != 0) {
             root.findPreference(KEY_SCREEN_PINNING).setSummary(
                     getResources().getString(R.string.switch_on_text));
-        }
-
-	final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
-	PreferenceScreen prefSet = getPreferenceScreen();
-        mBlockOnSecureKeyguard = (SwitchPreference) findPreference(PREF_BLOCK_ON_SECURE_KEYGUARD);
-        if (lockPatternUtils.isSecure()) {
-            mBlockOnSecureKeyguard.setChecked(Settings.Secure.getInt(getContentResolver(),
-                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 1) == 1);
-            mBlockOnSecureKeyguard.setOnPreferenceChangeListener(this);
-        } else if (mBlockOnSecureKeyguard != null) {
-            prefSet.removePreference(mBlockOnSecureKeyguard);
         }
 
         // Show password
@@ -794,11 +780,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
                     Integer.valueOf((String) value));
             mAdvancedReboot.setValue(String.valueOf(value));
             mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
-        } else if (preference == mBlockOnSecureKeyguard) {
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
+        } else if (preference == mQuickUnlockScreen) {
+            Settings.Secure.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
                     (Boolean) value ? 1 : 0);
-	}
+        }
         return result;
     }
 
