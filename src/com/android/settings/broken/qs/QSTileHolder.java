367dbfbd27ab299338699a70603d53125630d01b
CrackerTeg <crackerteg@gmail.com>
2015-07-15 23:40:18 -0500

    QS: Performance profile tile [1/2] Based on CM code but trimmed some of the fat
    Signed-off-by: CrackerTeg <crackerteg@gmail.com>

--- src/com/android/settings/broken/qs/QSTileHolder.java
+++ src/com/android/settings/broken/qs/QSTileHolder.java
@@ -106,6 +106,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_tile_notifications;
                 break;
+            case QSConstants.TILE_PERFORMANCE:
+                resourceName = "ic_qs_perf_profile_highperf_avd";
+                stringId = R.string.qs_tile_performance;
+                break;
             case QSConstants.TILE_NFC:
                 resourceName = "ic_qs_nfc_on";
                 stringId = R.string.qs_tile_nfc;
