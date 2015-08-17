82aed8a8fdf6641dd3bc5f215c4c423c0a8fea4c
stofstik <verstegen.daan@gmail.com>
2015-08-17 17:44:13 +0200

    Added show volume to tile chooser
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/broken/qs/QSTileHolder.java
+++ src/com/android/settings/broken/qs/QSTileHolder.java
@@ -131,6 +131,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_power";
                 stringId = R.string.qs_screen_off_tile;
                 break;
+            case QSConstants.TILE_VOLUME:
+                resourceName = "ic_qs_volume_panel";
+                stringId = R.string.qs_tile_volume_panel;
+                break;
             default:
                 return null;
         }
