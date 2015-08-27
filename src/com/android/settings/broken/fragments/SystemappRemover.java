DirtyUnicorns/android_packages_apps_DU-Tweaks

Fix system app remover to work in 5.1
Change-Id: Id940aa9b128f914c222448198856a8ac12e6b7af

src/com/dirtyunicorns/dutweaks/fragments/SystemappRemover.java
@@ -281,7 +281,7 @@ public class SystemappRemover extends Fragment {
                         // of the selected item
                         short state = sdAvailable();
                         File path = new File(Environment
-                                .getExternalStorageDirectory() + "/aicp");
+                                .getExternalStorageDirectory() + "/du");
                         File savefile = new File(path + "/systemappremover.stf");
                         if (which == 0) {
                             // load profile action
@@ -432,17 +432,17 @@ public class SystemappRemover extends Fragment {
             for (String appName : params) {
                 String odexAppName = appName.replaceAll(".apk$", ".odex");
                 String basePath = systemPath;
-                File app = new File(systemPath + appName);
+                File app = new File(systemPath);
 
                 if( ! app.exists() )
                     basePath = systemPrivPath;
 
                 try {
-                    dos.writeBytes("\n" + "rm -f '" + basePath + appName + "'\n");
+                    dos.writeBytes("\n" + "rm -rf '" + basePath + "*" + appName + "'\n");
                     // needed in case user is using odexed ROM
                     File odex = new File(basePath + odexAppName);
                     if( odex.exists() )
-                        dos.writeBytes("\n" + "rm -f '" + basePath + odexAppName + "'\n");
+                        dos.writeBytes("\n" + "rm -rf '" + basePath + odexAppName + "'\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }

DysfunctionalROMs/packages_apps_Settings

src/com/android/settings/broken/fragments/SystemappRemover.java
@@ -281,7 +281,7 @@ public class SystemappRemover extends Fragment {
                         // of the selected item
                         short state = sdAvailable();
                         File path = new File(Environment
-                                .getExternalStorageDirectory() + "/aicp");
+                                .getExternalStorageDirectory() + "/broken");
                         File savefile = new File(path + "/systemappremover.stf");
                         if (which == 0) {
                             // load profile action
@@ -432,17 +432,17 @@ public class SystemappRemover extends Fragment {
             for (String appName : params) {
                 String odexAppName = appName.replaceAll(".apk$", ".odex");
                 String basePath = systemPath;
-                File app = new File(systemPath + appName);
+                File app = new File(systemPath);
 
                 if( ! app.exists() )
                     basePath = systemPrivPath;
 
                 try {
-                    dos.writeBytes("\n" + "rm -f '" + basePath + appName + "'\n");
+                    dos.writeBytes("\n" + "rm -rf '" + basePath + "*" + appName + "'\n");
                     // needed in case user is using odexed ROM
                     File odex = new File(basePath + odexAppName);
                     if( odex.exists() )
-                        dos.writeBytes("\n" + "rm -f '" + basePath + odexAppName + "'\n");
+                        dos.writeBytes("\n" + "rm -rf '" + basePath + odexAppName + "'\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }