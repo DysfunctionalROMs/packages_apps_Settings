siracuervo/android_packages_apps_Settings-4

Lockscreen shortcut customizations, (2/2):
Based on Cyanogenmod implementation:

add Voice Wakeup switch
(only shortcut picker related parts):
https://github.com/CyanogenMod/android_packages_apps_Settings/commit/83cc29347ba93d53c00a441d857fdd588c37c035

Settings : Lockscreen shortcuts:
https://github.com/CyanogenMod/android_packages_apps_Settings/commit/170400a097c7f8770487ac82a17fcc4a88fb3bd4

Settings : Add ability to disable a lockscreen shortcut:
https://github.com/CyanogenMod/android_packages_apps_Settings/commit/2a3fcb2c82ba9cddafe5e8de3e0e594547367215

Additions:
- Material (dark) theme

See frameworks part for more infos:
https://github.com/XXMrHyde/android_frameworks_base/commit/3194062f0ed6ef204658872e29bbe710cd0c2fbd

src/com/android/settings/cyanogenmod/GlowBackground.java
@@ -1,0 +1,82 @@
+package com.android.settings.cyanogenmod;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.view.animation.AnticipateOvershootInterpolator;
+
+public class GlowBackground extends Drawable implements ValueAnimator.AnimatorUpdateListener {
+
+    private static final int MAX_CIRCLE_SIZE = 150;
+
+    private final Paint mPaint;
+    private Animator mAnimator;
+    private float mCircleSize;
+
+    public GlowBackground(int color) {
+        mPaint = new Paint();
+        mPaint.setColor(color);
+    }
+
+    private void startAnimation(boolean hide) {
+        if (mAnimator != null) {
+            mAnimator.cancel();
+        }
+        if (hide && mCircleSize == 0f) {
+            return;
+        } else if (!hide && mCircleSize == MAX_CIRCLE_SIZE) {
+            return;
+        }
+        mAnimator = getAnimator(hide);
+        mAnimator.start();
+    }
+
+    private Animator getAnimator(boolean hide) {
+        float start = mCircleSize;
+        float end = MAX_CIRCLE_SIZE;
+        if (hide) {
+            end = 0f;
+        }
+        ValueAnimator animator = ObjectAnimator.ofFloat(start, end);
+        animator.setInterpolator(new AnticipateOvershootInterpolator());
+        animator.setDuration(300);
+        animator.addUpdateListener(this);
+        return animator;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(getBounds().width() / 2, getBounds().height() / 2, mCircleSize, mPaint);
+    }
+
+    @Override
+    public void setAlpha(int i) {
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mCircleSize = (Float) valueAnimator.getAnimatedValue();
+        invalidateSelf();
+    }
+
+    public void hideGlow() {
+        startAnimation(true);
+    }
+
+    public void showGlow() {
+        startAnimation(false);
+    }
+}

src/com/android/settings/darkkat/LockScreenShortcutsSettings.java
@@ -1,0 +1,62 @@
+/* 
+ * Copyright (C) 2015 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.darkkat;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockScreenShortcutsSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_COLORIZE_CUSTOM_ICONS =
+            "shortcuts_colorize_custom_icons";
+
+    private CheckBoxPreference mColorizeCustomIcons;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lock_screen_shortcuts_settings);
+        mResolver = getActivity().getContentResolver();
+
+        mColorizeCustomIcons =
+                (CheckBoxPreference) findPreference(PREF_COLORIZE_CUSTOM_ICONS);
+        mColorizeCustomIcons.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS, 0) == 1);
+        mColorizeCustomIcons.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mColorizeCustomIcons) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}

src/com/android/settings/darkkat/LockscreenShortcuts.java
@@ -1,0 +1,246 @@
+package com.android.settings.darkkat;
+
+import android.annotation.Nullable;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.android.internal.util.darkkat.LockscreenShortcutsHelper;
+import com.android.settings.R;
+
+import com.android.settings.cyanogenmod.GlowBackground;
+import com.android.settings.cyanogenmod.ShortcutPickHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LockscreenShortcuts extends Fragment implements View.OnClickListener,
+        ShortcutPickHelper.OnPickListener, View.OnTouchListener {
+
+    private static final int[] sIconIds = new int[]{R.id.left_button, R.id.right_button};
+    private static final String ACTION_APP = "action_app";
+
+    private ActionHolder mActions;
+    private ShortcutPickHelper mPicker;
+    private LockscreenShortcutsHelper mShortcutHelper;
+    private View mSelectedView;
+    private ColorStateList mDefaultTintList;
+
+    @Override
+    public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
+        onTargetChange(uri);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                GlowBackground background = (GlowBackground) v.getBackground();
+                background.showGlow();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                background = (GlowBackground) v.getBackground();
+                background.hideGlow();
+                break;
+        }
+        return false;
+    }
+
+    private class ActionHolder {
+        private ArrayList<CharSequence> mAvailableEntries = new ArrayList<CharSequence>();
+        private ArrayList<String> mAvailableValues = new ArrayList<String>();
+
+        public void addAction(String action, int entryResId) {
+            mAvailableEntries.add(getString(entryResId));
+            mAvailableValues.add(action);
+        }
+
+        public int getActionIndex(String action) {
+            int count = mAvailableValues.size();
+            for (int i = 0; i < count; i++) {
+                if (TextUtils.equals(mAvailableValues.get(i), action)) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public String getAction(int index) {
+            if (index > mAvailableValues.size()) {
+                return null;
+            }
+
+            return mAvailableValues.get(index);
+        }
+
+        public CharSequence[] getEntries() {
+            return mAvailableEntries.toArray(new CharSequence[mAvailableEntries.size()]);
+        }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.lockscreen_shortcuts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPicker = new ShortcutPickHelper(getActivity(), this);
+        mShortcutHelper = new LockscreenShortcutsHelper(getActivity(), null);
+        createActionList();
+        initiateViews(view);
+        setUnlockIcon(view);
+        updateDrawables();
+    }
+
+    private void setUnlockIcon(View view) {
+        ImageView unlock = (ImageView) view.findViewById(R.id.middle_button);
+        mDefaultTintList = unlock.getImageTintList();
+        Drawable d = mShortcutHelper.getDrawableFromSystemUI("ic_lock_24dp");
+        if (d != null) {
+            unlock.setImageDrawable(d);
+            unlock.setColorFilter(getIconColor(), Mode.SRC_ATOP);
+        }
+    }
+
+    private int getIconColor() {
+        int color = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.LOCK_SCREEN_ICON_COLOR, 0xffffffff);
+        return color;
+    }
+
+    private void initiateViews(View view) {
+        int size = getResources().getDimensionPixelSize(R.dimen.lockscreen_icon_size);
+        for (int id : sIconIds) {
+            View v = view.findViewById(id);
+            v.setOnClickListener(this);
+            v.setOnTouchListener(this);
+            GlowBackground background = new GlowBackground(getIconColor());
+            background.setBounds(0, 0, size, size);
+            v.setBackground(background);
+        }
+    }
+
+    private void updateDrawables() {
+        List<LockscreenShortcutsHelper.TargetInfo> items = mShortcutHelper.getDrawablesForTargets();
+        for (int i = 0; i < sIconIds.length; i++) {
+            LockscreenShortcutsHelper.TargetInfo item = items.get(i);
+            int id = sIconIds[i];
+            ImageView v = (ImageView) getView().findViewById(id);
+            if (LockscreenShortcutsHelper.NONE.equals(item.uri)) {
+                v.setImageResource(R.drawable.ic_lockscreen_shortcuts_blank);
+            } else {
+                v.setImageDrawable(item.icon);
+            }
+            v.setTag(item.uri);
+            if (LockscreenShortcutsHelper.DEFAULT.equals(item.uri) ||
+                    LockscreenShortcutsHelper.NONE.equals(item.uri)) {
+                v.setColorFilter(getIconColor(), Mode.SRC_ATOP);
+            } else {
+                v.setColorFilter(null);
+            }
+        }
+    }
+
+    private void createActionList() {
+        mActions = new ActionHolder();
+        mActions.addAction(LockscreenShortcutsHelper.NONE, R.string.shortcuts_none_target);
+        mActions.addAction(LockscreenShortcutsHelper.DEFAULT, R.string.shortcuts_default_target);
+        mActions.addAction(ACTION_APP, R.string.shortcuts_custom_target);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPicker.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        mSelectedView = v;
+
+        final GlowBackground background = (GlowBackground) mSelectedView.getBackground();
+
+        mSelectedView.postOnAnimation(new Runnable() {
+            @Override
+            public void run() {
+                background.showGlow();
+            }
+        });
+
+        final DialogInterface.OnClickListener l = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                onTargetChange(mActions.getAction(item));
+                dialog.dismiss();
+            }
+        };
+
+        final DialogInterface.OnCancelListener cancel = new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                onTargetChange(null);
+            }
+        };
+
+        final AlertDialog dialog = new AlertDialog.Builder(
+                getActivity(), AlertDialog.THEME_MATERIAL_DARK)
+                .setTitle(R.string.shortcuts_reassign_shortcut_title)
+                .setItems(mActions.getEntries(), l)
+                .setOnCancelListener(cancel)
+                .create();
+
+        dialog.show();
+    }
+
+    private void onTargetChange(String uri) {
+        if (uri == null) {
+            final GlowBackground background = (GlowBackground) mSelectedView.getBackground();
+            background.hideGlow();
+            return;
+        }
+
+        if (uri.equals(ACTION_APP)) {
+            mPicker.pickShortcut(null, null, getId());
+        } else {
+            mSelectedView.setTag(uri);
+            saveCustomActions();
+            updateDrawables();
+            mSelectedView.postOnAnimation(new Runnable() {
+                @Override
+                public void run() {
+                    final GlowBackground background = (GlowBackground) mSelectedView.getBackground();
+                    background.hideGlow();
+                }
+            });
+        }
+    }
+
+    private void saveCustomActions() {
+        ArrayList<String> targets = new ArrayList<String>();
+        for (int id : sIconIds) {
+            View v = getView().findViewById(id);
+            String uri = (String) v.getTag();
+            targets.add(uri);
+        }
+        mShortcutHelper.saveTargets(targets);
+    }
+
+}

DysfunctionalROMs/packages_apps_Settings

Conflicts:
	res/color/audio_icon.xml
	res/values-de/dk_strings.xml
	res/values/dk_strings.xml
	res/xml/lock_screen.xml

src/com/android/settings/broken/GlowBackground.java
@@ -1,0 +1,82 @@
+package com.android.settings.broken;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.view.animation.AnticipateOvershootInterpolator;
+
+public class GlowBackground extends Drawable implements ValueAnimator.AnimatorUpdateListener {
+
+    private static final int MAX_CIRCLE_SIZE = 150;
+
+    private final Paint mPaint;
+    private Animator mAnimator;
+    private float mCircleSize;
+
+    public GlowBackground(int color) {
+        mPaint = new Paint();
+        mPaint.setColor(color);
+    }
+
+    private void startAnimation(boolean hide) {
+        if (mAnimator != null) {
+            mAnimator.cancel();
+        }
+        if (hide && mCircleSize == 0f) {
+            return;
+        } else if (!hide && mCircleSize == MAX_CIRCLE_SIZE) {
+            return;
+        }
+        mAnimator = getAnimator(hide);
+        mAnimator.start();
+    }
+
+    private Animator getAnimator(boolean hide) {
+        float start = mCircleSize;
+        float end = MAX_CIRCLE_SIZE;
+        if (hide) {
+            end = 0f;
+        }
+        ValueAnimator animator = ObjectAnimator.ofFloat(start, end);
+        animator.setInterpolator(new AnticipateOvershootInterpolator());
+        animator.setDuration(300);
+        animator.addUpdateListener(this);
+        return animator;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(getBounds().width() / 2, getBounds().height() / 2, mCircleSize, mPaint);
+    }
+
+    @Override
+    public void setAlpha(int i) {
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mCircleSize = (Float) valueAnimator.getAnimatedValue();
+        invalidateSelf();
+    }
+
+    public void hideGlow() {
+        startAnimation(true);
+    }
+
+    public void showGlow() {
+        startAnimation(false);
+    }
+}

src/com/android/settings/broken/LockScreenShortcutsSettings.java
@@ -1,0 +1,62 @@
+/* 
+ * Copyright (C) 2015 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.broken;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockScreenShortcutsSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_COLORIZE_CUSTOM_ICONS =
+            "shortcuts_colorize_custom_icons";
+
+    private SwitchPreference mColorizeCustomIcons;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lock_screen_shortcuts_settings);
+        mResolver = getActivity().getContentResolver();
+
+        mColorizeCustomIcons =
+                (SwitchPreference) findPreference(PREF_COLORIZE_CUSTOM_ICONS);
+        mColorizeCustomIcons.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS, 0) == 1);
+        mColorizeCustomIcons.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mColorizeCustomIcons) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}

src/com/android/settings/broken/LockscreenShortcuts.java
@@ -1,0 +1,245 @@
+package com.android.settings.broken;
+
+import android.annotation.Nullable;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.android.internal.util.broken.LockscreenShortcutsHelper;
+import com.android.settings.R;
+
+import com.android.settings.broken.GlowBackground;
+import com.android.settings.broken.ShortcutPickHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LockscreenShortcuts extends Fragment implements View.OnClickListener,
+        ShortcutPickHelper.OnPickListener, View.OnTouchListener {
+
+    private static final int[] sIconIds = new int[]{R.id.left_button, R.id.right_button};
+    private static final String ACTION_APP = "action_app";
+
+    private ActionHolder mActions;
+    private ShortcutPickHelper mPicker;
+    private LockscreenShortcutsHelper mShortcutHelper;
+    private View mSelectedView;
+    private ColorStateList mDefaultTintList;
+
+    @Override
+    public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
+        onTargetChange(uri);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                GlowBackground background = (GlowBackground) v.getBackground();
+                background.showGlow();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                background = (GlowBackground) v.getBackground();
+                background.hideGlow();
+                break;
+        }
+        return false;
+    }
+
+    private class ActionHolder {
+        private ArrayList<CharSequence> mAvailableEntries = new ArrayList<CharSequence>();
+        private ArrayList<String> mAvailableValues = new ArrayList<String>();
+
+        public void addAction(String action, int entryResId) {
+            mAvailableEntries.add(getString(entryResId));
+            mAvailableValues.add(action);
+        }
+
+        public int getActionIndex(String action) {
+            int count = mAvailableValues.size();
+            for (int i = 0; i < count; i++) {
+                if (TextUtils.equals(mAvailableValues.get(i), action)) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public String getAction(int index) {
+            if (index > mAvailableValues.size()) {
+                return null;
+            }
+
+            return mAvailableValues.get(index);
+        }
+
+        public CharSequence[] getEntries() {
+            return mAvailableEntries.toArray(new CharSequence[mAvailableEntries.size()]);
+        }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.lockscreen_shortcuts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPicker = new ShortcutPickHelper(getActivity(), this);
+        mShortcutHelper = new LockscreenShortcutsHelper(getActivity(), null);
+        createActionList();
+        initiateViews(view);
+        setUnlockIcon(view);
+        updateDrawables();
+    }
+
+    private void setUnlockIcon(View view) {
+        ImageView unlock = (ImageView) view.findViewById(R.id.middle_button);
+        mDefaultTintList = unlock.getImageTintList();
+        Drawable d = mShortcutHelper.getDrawableFromSystemUI("ic_lock_24dp");
+        if (d != null) {
+            unlock.setImageDrawable(d);
+            unlock.setColorFilter(getIconColor(), Mode.SRC_ATOP);
+        }
+    }
+
+    private int getIconColor() {
+        int color = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.LOCK_SCREEN_ICON_COLOR, 0xffffffff);
+        return color;
+    }
+
+    private void initiateViews(View view) {
+        int size = getResources().getDimensionPixelSize(R.dimen.lockscreen_icon_size);
+        for (int id : sIconIds) {
+            View v = view.findViewById(id);
+            v.setOnClickListener(this);
+            v.setOnTouchListener(this);
+            GlowBackground background = new GlowBackground(getIconColor());
+            background.setBounds(0, 0, size, size);
+            v.setBackground(background);
+        }
+    }
+
+    private void updateDrawables() {
+        List<LockscreenShortcutsHelper.TargetInfo> items = mShortcutHelper.getDrawablesForTargets();
+        for (int i = 0; i < sIconIds.length; i++) {
+            LockscreenShortcutsHelper.TargetInfo item = items.get(i);
+            int id = sIconIds[i];
+            ImageView v = (ImageView) getView().findViewById(id);
+            if (LockscreenShortcutsHelper.NONE.equals(item.uri)) {
+                v.setImageResource(R.drawable.ic_lockscreen_shortcuts_blank);
+            } else {
+                v.setImageDrawable(item.icon);
+            }
+            v.setTag(item.uri);
+            if (LockscreenShortcutsHelper.DEFAULT.equals(item.uri) ||
+                    LockscreenShortcutsHelper.NONE.equals(item.uri)) {
+                v.setColorFilter(getIconColor(), Mode.SRC_ATOP);
+            } else {
+                v.setColorFilter(null);
+            }
+        }
+    }
+
+    private void createActionList() {
+        mActions = new ActionHolder();
+        mActions.addAction(LockscreenShortcutsHelper.NONE, R.string.shortcuts_none_target);
+        mActions.addAction(LockscreenShortcutsHelper.DEFAULT, R.string.shortcuts_default_target);
+        mActions.addAction(ACTION_APP, R.string.shortcuts_custom_target);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPicker.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        mSelectedView = v;
+
+        final GlowBackground background = (GlowBackground) mSelectedView.getBackground();
+
+        mSelectedView.postOnAnimation(new Runnable() {
+            @Override
+            public void run() {
+                background.showGlow();
+            }
+        });
+
+        final DialogInterface.OnClickListener l = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                onTargetChange(mActions.getAction(item));
+                dialog.dismiss();
+            }
+        };
+
+        final DialogInterface.OnCancelListener cancel = new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                onTargetChange(null);
+            }
+        };
+
+        final AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.shortcuts_reassign_shortcut_title)
+                .setItems(mActions.getEntries(), l)
+                .setOnCancelListener(cancel)
+                .create();
+
+        dialog.show();
+    }
+
+    private void onTargetChange(String uri) {
+        if (uri == null) {
+            final GlowBackground background = (GlowBackground) mSelectedView.getBackground();
+            background.hideGlow();
+            return;
+        }
+
+        if (uri.equals(ACTION_APP)) {
+            mPicker.pickShortcut(null, null, getId());
+        } else {
+            mSelectedView.setTag(uri);
+            saveCustomActions();
+            updateDrawables();
+            mSelectedView.postOnAnimation(new Runnable() {
+                @Override
+                public void run() {
+                    final GlowBackground background = (GlowBackground) mSelectedView.getBackground();
+                    background.hideGlow();
+                }
+            });
+        }
+    }
+
+    private void saveCustomActions() {
+        ArrayList<String> targets = new ArrayList<String>();
+        for (int id : sIconIds) {
+            View v = getView().findViewById(id);
+            String uri = (String) v.getTag();
+            targets.add(uri);
+        }
+        mShortcutHelper.saveTargets(targets);
+    }
+
+}