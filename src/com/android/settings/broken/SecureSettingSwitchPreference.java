7d934b4e3a7e7dbfd0d3a49d1c08343a40902bd6
InkSlinger666 <kengreenwood420666@gmail.com>
2015-11-10 12:42:37 -0600

    Merge derp

--- src/com/android/settings/broken/SecureSettingSwitchPreference.java
+++ src/com/android/settings/broken/SecureSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.chroma;
+package com.android.settings.broken;
 
 import android.content.Context;
 import android.preference.SwitchPreference;
