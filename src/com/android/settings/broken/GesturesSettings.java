08a7e7141101e5116df9e3b7b4ef7c454095c49d
InkSlinger666 <kengreenwood420666@gmail.com>
2015-11-10 11:03:54 -0600

    Settings up Broken Pieces subsections

---
+++ src/com/android/settings/broken/GesturesSettings.java
@@ -0,0 +1,16 @@
+package com.android.settings.broken;
+
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class GestureSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.broken_settings_gestures);
+    }
+}
