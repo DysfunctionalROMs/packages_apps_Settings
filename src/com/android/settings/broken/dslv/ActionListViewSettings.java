3a13b2c0d1f99fd98d02fd1cbdd608fdc90af7dd
InkSlinger666 <kengreenwood420666@gmail.com>
2016-01-20 12:27:15 -0600

    Fix nav gallery selection... Hints taken from Screwd

--- src/com/android/settings/broken/dslv/ActionListViewSettings.java
+++ src/com/android/settings/broken/dslv/ActionListViewSettings.java
@@ -213,10 +213,7 @@ public class ActionListViewSettings extends ListFragment implements
 
         mPicker = new ShortcutPickerHelper(mActivity, this);
 
-        File folder = new File(Environment.getExternalStorageDirectory() + File.separator +
-                ".broken" + File.separator + "icons");
-
-        mImageTmp = new File(folder.toString()
+        mImageTmp = new File(mActivity.getCacheDir()
                 + File.separator + "shortcut.tmp");
 
         DragSortListView listView = (DragSortListView) getListView();
@@ -336,23 +333,18 @@ public class ActionListViewSettings extends ListFragment implements
         }
         if (bmp != null && !mPendingLongpress) {
             // Icon is present, save it for future use and add the file path to the action.
-            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                File folder = new File(Environment.getExternalStorageDirectory() + File.separator +
-                        ".broken" + File.separator + "icons");
-                folder.mkdirs();
-                String fileName = folder.toString()
-                        + File.separator + "shortcut_" + System.currentTimeMillis() + ".png";
-                try {
-                    FileOutputStream out = new FileOutputStream(fileName);
-                    bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
-                    out.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    action = action + "?hasExtraIcon=" + fileName;
-                    File image = new File(fileName);
-                    image.setReadable(true, false);
-                }
+            String fileName = mActivity.getFilesDir()
+                     + File.separator + "shortcut_" + System.currentTimeMillis() + ".png";
+             try {
+                 FileOutputStream out = new FileOutputStream(fileName);
+                 bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
+                 out.close();
+             } catch (Exception e) {
+                 e.printStackTrace();
+             } finally {
+                 action = action + "?hasExtraIcon=" + fileName;
+                 File image = new File(fileName);
+                 image.setReadable(true, false);
             }
         }
         if (mPendingNewAction) {
@@ -380,18 +372,13 @@ public class ActionListViewSettings extends ListFragment implements
                             Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                    File folder = new File(Environment.getExternalStorageDirectory() +
-                            File.separator + ".broken" + File.separator + "icons");
-                    folder.mkdirs();
-                    File image = new File(folder.toString() + File.separator
-                            + "shortcut_" + System.currentTimeMillis() + ".png");
-                    String path = image.getAbsolutePath();
-                    mImageTmp.renameTo(image);
-                    image.setReadable(true, false);
-                    updateAction(null, null, path, mPendingIndex, false);
-                    mPendingIndex = -1;
-                }
+                File image = new File(mActivity.getFilesDir() + File.separator
+                        + "shortcut_" + System.currentTimeMillis() + ".png");
+                String path = image.getAbsolutePath();
+                mImageTmp.renameTo(image);
+                image.setReadable(true, false);
+                updateAction(null, null, path, mPendingIndex, false);
+                mPendingIndex = -1;
             }
         } else {
             if (mImageTmp.exists()) {
