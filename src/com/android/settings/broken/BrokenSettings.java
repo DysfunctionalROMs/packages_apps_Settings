d318eabf1e20f627374be442cba52efd7cee595e
Alex Cruz <mazdarider23@gmail.com>
2016-02-03 16:26:51 -0500

    Selinux Switch [1/2]
    Change-Id: I845322877a58bfebca239e72c8ef803f76c851e9
    
    Conflicts:
    	res/xml/misctweaks.xml
    	src/com/dirtyunicorns/dutweaks/fragments/MiscTweaks.java

--- src/com/android/settings/broken/BrokenSettings.java
+++ src/com/android/settings/broken/BrokenSettings.java
@@ -1,11 +1,34 @@
 
 package com.android.settings.broken;
 
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
-import com.android.settings.InstrumentedFragment;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import android.provider.Settings.SettingNotFoundException;
+import com.android.internal.util.broken.AbstractAsyncSuCMDProcessor;
+import com.android.internal.util.broken.CMDProcessor;
+import com.android.internal.util.broken.Helpers;
+import com.android.internal.util.broken.BrokenUtils;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.DataOutputStream;
 
 public class BrokenSettings extends SettingsPreferenceFragment {
 
@@ -14,10 +37,45 @@ public class BrokenSettings extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.broken_settings);
+        
+        final ContentResolver resolver = getActivity().getContentResolver();
+	    final PreferenceScreen prefScreen = getPreferenceScreen();
+	    
+	    //SELinux
+        mSelinux = (SwitchPreference) findPreference(SELINUX);
+        mSelinux.setOnPreferenceChangeListener(this);
+
+        if (CMDProcessor.runShellCommand("getenforce").getStdout().contains("Enforcing")) {
+            mSelinux.setChecked(true);
+            mSelinux.setSummary(R.string.selinux_enforcing_title);
+        } else {
+            mSelinux.setChecked(false);
+            mSelinux.setSummary(R.string.selinux_permissive_title);
+        }
     }
     
     @Override
     protected int getMetricsCategory() {
         return InstrumentedFragment.BROKENSETTINGS;
     }
- }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+    
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		if (preference == mSelinux) {
+            if (newValue.toString().equals("true")) {
+                CMDProcessor.runSuCommand("setenforce 1");
+                mSelinux.setSummary(R.string.selinux_enforcing_title);
+            } else if (newValue.toString().equals("false")) {
+                CMDProcessor.runSuCommand("setenforce 0");
+                mSelinux.setSummary(R.string.selinux_permissive_title);
+            }
+            return true;
+        }
+        return false;
+    }
+}
