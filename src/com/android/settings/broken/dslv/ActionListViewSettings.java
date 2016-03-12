eaafda6e6deb7932f2f27c3bc1417bf024460d5a
InkSlinger666 <kengreenwood420666@gmail.com>
2016-03-12 03:47:46 -0600

    A few more changes for custom icons in DSLV

--- src/com/android/settings/broken/dslv/ActionListViewSettings.java
+++ src/com/android/settings/broken/dslv/ActionListViewSettings.java
@@ -656,15 +656,18 @@ public class ActionListViewSettings extends ListFragment implements
                         mActivity, ActionHelper.getActionIconImage(mActivity,
                         getItem(position).getClickAction(),
                         iconUri), 36);
-            }
 
-            if ((iconUri.equals(ActionConstants.ICON_EMPTY) &&
-                    getItem(position).getClickAction().startsWith("**")) || (iconUri != null
-                    && iconUri.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER))) {
-                if (d != null) {
-                    d = ImageHelper.getColoredDrawable(d,
-                            getResources().getColor(R.color.dslv_icon_dark));
+                if ((iconUri.equals(ActionConstants.ICON_EMPTY) &&
+                        getItem(position).getClickAction().startsWith("**")) || (iconUri != null
+                        && iconUri.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER))) {
+                    if (d != null) {
+                        d = ImageHelper.getColoredDrawable(d,
+                                getResources().getColor(R.color.dslv_icon_dark));
+                    }
                 }
+            }
+            if (iconUri != null && iconUri.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER)) {
+                d.setTint(getResources().getColor(R.color.dslv_icon_dark));
             }
             holder.iconView.setImageBitmap(ImageHelper.drawableToBitmap(d));
 
