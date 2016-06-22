8be1569da7daa603c53a036deddb00cfa59fd5e5
LorDClockaN <lordclockan@gmail.com>
2016-06-22 15:15:00 -0500

    Settings: QSStroke: Don't apply any effect when disabled (2/2)
    Border radius is useless now, so disable it when stroke
    is disabled
    
    Change-Id: I9007f671ae2a5ee04b68848c077bdc18d5d6ea64

--- src/com/android/settings/broken/QsSettingsAdvanced.java
+++ src/com/android/settings/broken/QsSettingsAdvanced.java
@@ -286,12 +286,15 @@ public class QsSettingsAdvanced extends SettingsPreferenceFragment implements On
         if (qSStroke == 0) {
             mQSStrokeColor.setEnabled(false);
             mQSStrokeThickness.setEnabled(false);
+            mQSCornerRadius.setEnabled(false);
         } else if (qSStroke == 1) {
             mQSStrokeColor.setEnabled(false);
             mQSStrokeThickness.setEnabled(true);
+            mQSCornerRadius.setEnabled(true);
         } else {
             mQSStrokeColor.setEnabled(true);
             mQSStrokeThickness.setEnabled(true);
+            mQSCornerRadius.setEnabled(true);
         }
     }
 
