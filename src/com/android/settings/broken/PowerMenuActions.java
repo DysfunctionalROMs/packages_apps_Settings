75d3a4f5ef0a220389d0183fc9a08627192badd0
InkSlinger666 <kengreenwood420666@gmail.com>
2016-10-06 12:07:45 -0500

    Broken Pieces: Add to settings and setup the subsections Ported from Broken MM code Thanks to @camcory for showing the way with the new manifest setup Adapted from: https://github.com/AOSIP/platform_packages_apps_Settings/commit/ef70c73287c355e345f49ad07c40ea8299d3c4c8 and https://gerrit.omnirom.org/#/c/19432/

---
+++ src/com/android/settings/broken/PowerMenuActions.java
@@ -0,0 +1,36 @@
+package com.android.settings.broken;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.InstrumentedFragment;
+
+public class PowerMenuActions extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+// private variables here
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.broken_settings_power);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    // preference changes here
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return InstrumentedFragment.BROKENPOWER;
+    }
+}
